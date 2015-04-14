784090f64a7750f20c4912eb322c93b7d23b0bdd
QINGWEI LAN <qingweilan@gmail.com>
2015-04-13 17:41:02 -0700

    Initial Commit

---
+++ AirforU/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AirforU
+//
+//  Created by QINGWEI on 4/13/15.
+//  Copyright (c) 2015 QINGWEI LAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
