031cf834fc2f1a3762a3ed7bd4d8ca63f388fed8
QINGWEI LAN <qingweilan@gmail.com>
2015-06-06 23:53:49 -0700

    incorporated toxics fetch url without search utility

--- AirforU/ToxicsTab/ToxicsViewController.m
+++ AirforU/ToxicsTab/ToxicsViewController.m
@@ -8,17 +8,28 @@
 
 #import "ToxicsViewController.h"
 #import "FacilityCell.h"
+#import "AppDelegate.h"
 
 @interface ToxicsViewController () <UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate>
 
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (weak, nonatomic) IBOutlet UITextField *searchField;
 
+@property (nonatomic, strong) NSArray *results;
+
 @end
 
 @implementation ToxicsViewController
+{
+    NSString *zipcode;
+    BOOL shouldZipSearch;
+}
 
 #pragma mark - View Controller Life Cycle
+
+#define TOXICS_KEY_NAME @"name"
+#define TOXICS_KEY_TOTAL_RELEASES @"total_releases"
+#define TOXICS_KEY_DISTANCE @"distance"
 
 - (void)viewDidLoad
 {
@@ -37,9 +48,44 @@
                       [[UIBarButtonItem alloc] initWithTitle:@"Search" style:UIBarButtonItemStyleDone target:self action:@selector(search:)]];
     [toolBar sizeToFit];
     self.searchField.inputAccessoryView = toolBar;
+    
+    
+//    NSURL *url = [NSURL URLWithString:@"http://engage.environment.ucla.edu/airforu_tri.php?lat=34.072&long=-118.444"];
+//    NSData *data = [NSData dataWithContentsOfURL:url];
+//    NSArray *results = [NSJSONSerialization JSONObjectWithData:data options:0 error:NULL];
+    [self fetchCurrentResults];
+    
 }
 
 #pragma mark - Actions
+
+- (void)fetchCurrentResults
+{
+    AppDelegate *delegate = [[UIApplication sharedApplication] delegate];
+    
+    NSURL *url;
+    if (shouldZipSearch && zipcode)
+        url = [NSURL URLWithString:[NSString stringWithFormat:@"http://engage.environment.ucla.edu/airforu_tri.php?zip=%@", zipcode]];
+    else if (delegate.latitude && delegate.longitude)
+        url = [NSURL URLWithString:[NSString stringWithFormat:@"http://engage.environment.ucla.edu/airforu_tri.php?lat=%f&long=%f", delegate.latitude, delegate.longitude]];
+    else
+        url = [NSURL URLWithString:@"http://engage.environment.ucla.edu/airforu_tri.php?zip=90024"];
+    
+    dispatch_queue_t ToxicsQueue = dispatch_queue_create("ToxicsQueue", NULL);
+    dispatch_async(ToxicsQueue, ^{
+        NSData *data = [NSData dataWithContentsOfURL:url];
+        if (data) {
+            NSArray *arr = [NSJSONSerialization JSONObjectWithData:data options:0 error:NULL];
+            if (arr) {
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    self.results = arr;
+                    NSLog(@"%@", self.results);
+                    [self.tableView reloadData];
+                });
+            }
+        }
+    });
+}
 
 - (void)cancel:(UIBarButtonItem *)sender
 {
@@ -62,7 +108,7 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 5;
+    return self.results.count > 5 ? 5 : self.results.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -71,10 +117,21 @@
     
     FacilityCell *cell = [self.tableView dequeueReusableCellWithIdentifier:identifier];
     
+    NSDictionary *dict = self.results[indexPath.row];
+    
     /* Set cell properties */
-    cell.nameLabel.text = @"1. UCLA";
-    cell.distanceLabel.text = @"Distance: 50 mi";
-    cell.releaseLabel.text = @"Chemical Release (lbs): 32";
+    cell.nameLabel.text = [dict valueForKeyPath:TOXICS_KEY_NAME];
+    NSString *distance = [dict valueForKeyPath:TOXICS_KEY_DISTANCE];
+    if (!distance)
+        distance = @"?";
+    else {
+        double dis = [distance doubleValue];
+        dis += 0.5;
+        long cast = (long)dis;
+        distance = [NSString stringWithFormat:@"%ld", cast];
+    }
+    cell.distanceLabel.text = [NSString stringWithFormat:@"Distance: %@ mi",  distance];
+    cell.releaseLabel.text = [NSString stringWithFormat:@"Chemical Release (lbs): %@", [dict valueForKeyPath:TOXICS_KEY_TOTAL_RELEASES]];
     
     return cell;
 }
