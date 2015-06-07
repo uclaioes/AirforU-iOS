3fbdd1505b17a13f03b7fdb595a6122c055f14e3
QINGWEI LAN <qingweilan@gmail.com>
2015-06-06 23:13:08 -0700

    hardcoded font sizes for different iPhone screen sizes

--- AirforU/AirNowAPI/AQDimensions.h
+++ AirforU/AirNowAPI/AQDimensions.h
@@ -26,14 +26,13 @@ typedef enum {
 + (CGFloat)sizeForLocationLabelForPhoneType:(AQPhoneType)type;
 + (CGFloat)sizeForAQILabelForPhoneType:(AQPhoneType)type;
 + (CGFloat)sizeForAirQualityDescriptionForPhoneType:(AQPhoneType)type;
-+ (CGFloat)sizeForBehavorialQuestionForPhoneType:(AQPhoneType)type;
+//+ (CGFloat)sizeForBehavorialQuestionForPhoneType:(AQPhoneType)type;
 
 /* Historical View Page */
 + (CGFloat)sizeForWeekdayLabelsForPhoneType:(AQPhoneType)type;
 + (CGFloat)sizeForSmallAQIForPhoneType:(AQPhoneType)type;
 + (CGFloat)sizeForLargeAQIForPhoneType:(AQPhoneType)type;
 + (CGFloat)sizeForAverageTextLabelForPhoneType:(AQPhoneType)type;
-+ (CGFloat)sizeForAverageLabelForPhoneType:(AQPhoneType)type;
 + (CGFloat)sizeForAverageAirQualityDescriptionForPhoneType:(AQPhoneType)type;
 + (CGFloat)sizeForDisclaimerForPhoneType:(AQPhoneType)type;
 
--- AirforU/AirNowAPI/AQDimensions.m
+++ AirforU/AirNowAPI/AQDimensions.m
@@ -13,17 +13,24 @@
 /* Determine Phone Size */
 + (AQPhoneType)phoneTypeForScreenHeight:(CGFloat)height
 {
-    return IPHONE5;
+    if (height <= 510)
+        return IPHONE4;
+    else if (height <= 600)
+        return IPHONE5;
+    else if (height <= 700)
+        return IPHONE6;
+    else
+        return IPHONE6P;
 }
 
 /* Air Quality Page */
 + (CGFloat)sizeForContentLabelForPhoneType:(AQPhoneType)type
 {
     switch (type) {
-        case IPHONE4: return 0.0; break;
-        case IPHONE5: return 0.0; break;
-        case IPHONE6: return 0.0; break;
-        case IPHONE6P: return 0.0; break;
+        case IPHONE4: return 16.0; break;
+        case IPHONE5: return 18.0; break;
+        case IPHONE6: return 20.0; break;
+        case IPHONE6P: return 22.0; break;
         default: break;
     }
     
@@ -33,10 +40,10 @@
 + (CGFloat)sizeForLocationLabelForPhoneType:(AQPhoneType)type
 {
     switch (type) {
-        case IPHONE4: return 0.0; break;
-        case IPHONE5: return 0.0; break;
-        case IPHONE6: return 0.0; break;
-        case IPHONE6P: return 0.0; break;
+        case IPHONE4: return 19.0; break;
+        case IPHONE5: return 21.0; break;
+        case IPHONE6: return 24.0; break;
+        case IPHONE6P: return 27.0; break;
         default: break;
     }
     
@@ -46,10 +53,10 @@
 + (CGFloat)sizeForAQILabelForPhoneType:(AQPhoneType)type
 {
     switch (type) {
-        case IPHONE4: return 0.0; break;
-        case IPHONE5: return 0.0; break;
-        case IPHONE6: return 0.0; break;
-        case IPHONE6P: return 0.0; break;
+        case IPHONE4: return 38.0; break;
+        case IPHONE5: return 44.0; break;
+        case IPHONE6: return 54.0; break;
+        case IPHONE6P: return 66.0; break;
         default: break;
     }
     
@@ -59,37 +66,37 @@
 + (CGFloat)sizeForAirQualityDescriptionForPhoneType:(AQPhoneType)type
 {
     switch (type) {
-        case IPHONE4: return 0.0; break;
-        case IPHONE5: return 0.0; break;
-        case IPHONE6: return 0.0; break;
-        case IPHONE6P: return 0.0; break;
+        case IPHONE4: return 19.0; break;
+        case IPHONE5: return 21.0; break;
+        case IPHONE6: return 24.0; break;
+        case IPHONE6P: return 27.0; break;
         default: break;
     }
     
     return 0.0;
 }
 
-+ (CGFloat)sizeForBehavorialQuestionForPhoneType:(AQPhoneType)type
-{
-    switch (type) {
-        case IPHONE4: return 0.0; break;
-        case IPHONE5: return 0.0; break;
-        case IPHONE6: return 0.0; break;
-        case IPHONE6P: return 0.0; break;
-        default: break;
-    }
-    
-    return 0.0;
-}
+//+ (CGFloat)sizeForBehavorialQuestionForPhoneType:(AQPhoneType)type
+//{
+//    switch (type) {
+//        case IPHONE4: return 0.0; break;
+//        case IPHONE5: return 0.0; break;
+//        case IPHONE6: return 0.0; break;
+//        case IPHONE6P: return 0.0; break;
+//        default: break;
+//    }
+//    
+//    return 0.0;
+//}
 
 /* Historical View Page */
 + (CGFloat)sizeForWeekdayLabelsForPhoneType:(AQPhoneType)type
 {
     switch (type) {
-        case IPHONE4: return 0.0; break;
-        case IPHONE5: return 0.0; break;
-        case IPHONE6: return 0.0; break;
-        case IPHONE6P: return 0.0; break;
+        case IPHONE4: return 14.0; break;
+        case IPHONE5: return 15.0; break;
+        case IPHONE6: return 16.0; break;
+        case IPHONE6P: return 17.0; break;
         default: break;
     }
     
@@ -99,10 +106,10 @@
 + (CGFloat)sizeForSmallAQIForPhoneType:(AQPhoneType)type
 {
     switch (type) {
-        case IPHONE4: return 0.0; break;
-        case IPHONE5: return 0.0; break;
-        case IPHONE6: return 0.0; break;
-        case IPHONE6P: return 0.0; break;
+        case IPHONE4: return 9.0; break;
+        case IPHONE5: return 11.0; break;
+        case IPHONE6: return 12.0; break;
+        case IPHONE6P: return 13.0; break;
         default: break;
     }
     
@@ -112,10 +119,10 @@
 + (CGFloat)sizeForLargeAQIForPhoneType:(AQPhoneType)type
 {
     switch (type) {
-        case IPHONE4: return 0.0; break;
-        case IPHONE5: return 0.0; break;
-        case IPHONE6: return 0.0; break;
-        case IPHONE6P: return 0.0; break;
+        case IPHONE4: return 22.0; break;
+        case IPHONE5: return 26.0; break;
+        case IPHONE6: return 30.0; break;
+        case IPHONE6P: return 38.0; break;
         default: break;
     }
     
@@ -125,23 +132,10 @@
 + (CGFloat)sizeForAverageTextLabelForPhoneType:(AQPhoneType)type
 {
     switch (type) {
-        case IPHONE4: return 0.0; break;
-        case IPHONE5: return 0.0; break;
-        case IPHONE6: return 0.0; break;
-        case IPHONE6P: return 0.0; break;
-        default: break;
-    }
-    
-    return 0.0;
-}
-
-+ (CGFloat)sizeForAverageLabelForPhoneType:(AQPhoneType)type
-{
-    switch (type) {
-        case IPHONE4: return 0.0; break;
-        case IPHONE5: return 0.0; break;
-        case IPHONE6: return 0.0; break;
-        case IPHONE6P: return 0.0; break;
+        case IPHONE4: return 12.0; break;
+        case IPHONE5: return 14.0; break;
+        case IPHONE6: return 16.0; break;
+        case IPHONE6P: return 18.0; break;
         default: break;
     }
     
@@ -151,10 +145,10 @@
 + (CGFloat)sizeForAverageAirQualityDescriptionForPhoneType:(AQPhoneType)type
 {
     switch (type) {
-        case IPHONE4: return 0.0; break;
-        case IPHONE5: return 0.0; break;
-        case IPHONE6: return 0.0; break;
-        case IPHONE6P: return 0.0; break;
+        case IPHONE4: return 14.0; break;
+        case IPHONE5: return 16.0; break;
+        case IPHONE6: return 18.0; break;
+        case IPHONE6P: return 20.0; break;
         default: break;
     }
     
@@ -164,10 +158,10 @@
 + (CGFloat)sizeForDisclaimerForPhoneType:(AQPhoneType)type;
 {
     switch (type) {
-        case IPHONE4: return 0.0; break;
-        case IPHONE5: return 0.0; break;
-        case IPHONE6: return 0.0; break;
-        case IPHONE6P: return 0.0; break;
+        case IPHONE4: return 7.0; break;
+        case IPHONE5: return 8.0; break;
+        case IPHONE6: return 9.0; break;
+        case IPHONE6P: return 11.0; break;
         default: break;
     }
     
