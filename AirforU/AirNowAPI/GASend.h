0524be0e6359d8c309ef0b8863920228a76eaa91
QINGWEI LAN <qingweilan@gmail.com>
2015-07-06 20:14:21 +0800

    added wrapper for Google Analytics reporting

---
+++ AirforU/AirNowAPI/GASend.h
@@ -0,0 +1,20 @@
+//
+//  GASend.h
+//  AirforU
+//
+//  Created by Qingwei Lan on 7/6/15.
+//  Copyright (c) 2015 QINGWEI LAN. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "GAIDictionaryBuilder.h"
+#import "GAI.h"
+
+@interface GASend : NSObject
+
++ (void)sendEventWithAction:(NSString *)action;
+
++ (void)sendEventWithAction:(NSString *)action withLabel:(NSString *)label;
+
+@end
---
+++ AirforU/AirNowAPI/GASend.m
@@ -0,0 +1,39 @@
+//
+//  GASend.m
+//  AirforU
+//
+//  Created by Qingwei Lan on 7/6/15.
+//  Copyright (c) 2015 QINGWEI LAN. All rights reserved.
+//
+
+#import "GASend.h"
+
+@interface GASend ()
+
+@end
+
+@implementation GASend
+
++ (void)sendEventWithAction:(NSString *)action
+{
+    id tracker = [[GAI sharedInstance] defaultTracker];
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSString *identification = [defaults objectForKey:@"identification"];
+    
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateFormat:@"yyyy-MM-dd hh:mm:ss"];
+    NSString *timestamp = [formatter stringFromDate:[NSDate date]];
+    
+    [tracker send:[[GAIDictionaryBuilder createEventWithCategory:identification action:action label:timestamp value:nil] build]];
+}
+
++ (void)sendEventWithAction:(NSString *)action withLabel:(NSString *)label
+{
+    id tracker = [[GAI sharedInstance] defaultTracker];
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSString *identification = [defaults objectForKey:@"identification"];
+    
+    [tracker send:[[GAIDictionaryBuilder createEventWithCategory:identification action:action label:label value:nil] build]];
+}
+
+@end
